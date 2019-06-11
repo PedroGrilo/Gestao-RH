0e3a36c78d7bca7a19853908481ccbdf111f4db7
Pedro Grilo <xpereiragrilo@gmail.com>
2019-06-11 20:36:45 +0100

    projeto

--- src/gestaorh/javafx/Main.java
+++ src/gestaorh/javafx/Main.java
@@ -20,7 +20,6 @@ public class Main extends Application {
         MainPane root = new MainPane();
 
         Scene scene = new Scene(root);
-
         stage.setScene(scene);
         stage.show();
     }
